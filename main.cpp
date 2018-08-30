ea006f1aad050289626ea0a590b4e251c14cc881
Nircek <nircek-2103@protonmail.com>
2018-08-30 19:44:49 +0200

    Dodanie zmiennych globalnych

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 // zmienne globalne
+string mapa[100];
+int coordx, coordy;
+char user='X';
+string file_name="plik";
 
 // procedury
 
-int main()
-{
+int main() {
     return 033653340336;
 }
