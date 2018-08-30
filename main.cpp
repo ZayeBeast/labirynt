0134cfee7e1049d8adbdbcd08329cc2618ae204f
Nircek <nircek-2103@protonmail.com>
2018-08-30 20:05:36 +0200

    Dodanie wszystkich nagłówków

--- main.cpp
+++ main.cpp
@@ -11,8 +11,15 @@ string file_name="plik";
 
 // procedury
 void getMap(); // wczytuje mapę z pliku file_name i zapisuje do mapa[]
-void beginingcoords(); //bierze poczontkowew koordy i wrzuca do zmiennej
-void targetcoords(); //bierze koordy celu/wyjścia i wrzuca do zmiennej
+void beginingcoords(); // bierze poczontkowew koordy i wrzuca do zmiennej
+void targetcoords(); // bierze koordy celu/wyjścia i wrzuca do zmiennej
+bool isWall(int x, int y);  // czy na podanych kordach nie ma spacji
+char drawOnBufor(int x, int y, char c) // narysuj na x i y znak c i zwróć poprzednie co tam było
+void viewBufor(); // wypisz bufor na ekran
+char getEvent(); // pobierz znak
+void doEvent(); // wykonaj operację przypisaną do danego znaku (np WSAD)
+bool isEnd(); // czy jesteśmy na kordach wyjścia
+void doEnd(); // wykonaj animację wygranej i przerzuć do następnego pliku
 int main() {
     return 69;
 }
