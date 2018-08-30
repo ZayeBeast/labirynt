9a9ad400c4704ac380e547296ff6dacfa9ab7d12
MrDarkness19 <42713282+MrDarkness19@users.noreply.github.com>
2018-08-30 19:58:29 +0200

    nie v3

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ string file_name="plik";
 // procedury
 void getMap(); // wczytuje mapę z pliku file_name i zapisuje do mapa[]
 void beginingcoords(); //bierze poczontkowew koordy i wrzuca do zmiennej
+void targetcoords(); //bierze koordy celu/wyjścia i wrzuca do zmiennej
 int main() {
     return 69;
 }
