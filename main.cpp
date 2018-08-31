417151d445f5fe3d67cdcadc34972163ea338575
Crackhoff <42720407+Crackhoff@users.noreply.github.com>
2018-08-31 12:09:40 +0200

    RNG for coords of player

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 using namespace std;
 
@@ -11,7 +13,15 @@ string file_name="plik";
 
 // procedury
 void getMap(); // wczytuje mapę z pliku file_name i zapisuje do mapa[]
-void beginingcoords(); // bierze poczontkowew koordy i wrzuca do zmiennej
+void beginingcoords()
+{   do{
+        srand (time(NULL));
+            coordx = rand() % 100 + 1;
+        srand (time(NULL));
+            coordy = rand() % 100 + 1;
+        }while bool isWall(coordx, coordy) == true;
+ 
+    } // bierze poczontkowew koordy i wrzuca do zmiennej
 void targetcoords(); // bierze koordy celu/wyjścia i wrzuca do zmiennej
 bool isWall(int x, int y);  // czy na podanych kordach nie ma spacji
 char drawOnBufor(int x, int y, char c); // narysuj na x i y znak c i zwróć poprzednie co tam było
