bd4ae2664b27a68de46fbed79ffc39c911249e90
Nircek <nircek-2103@protonmail.com>
2018-08-31 12:53:52 +0200

    Dodanie szkieletu funkcji main()

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 // zmienne globalne
 string mapa[100];
+string bufor[100];
 int coordx, coordy;
 int endcoordx, endcoordy;
 char user='X';
@@ -15,6 +16,7 @@ void beginingcoords(); // bierze poczontkowew koordy i wrzuca do zmiennej
 void targetcoords(); // bierze koordy celu/wyjścia i wrzuca do zmiennej
 bool isWall(int x, int y);  // czy na podanych kordach nie ma spacji
 char drawOnBufor(int x, int y, char c); // narysuj na x i y znak c i zwróć poprzednie co tam było
+void refreshBufor(); // stwórz bufor
 void viewBufor(); // wypisz bufor na ekran
 char getEvent(); // pobierz znak
 void doEvent(); // wykonaj operację przypisaną do danego znaku (np WSAD)
@@ -22,7 +24,16 @@ bool isEnd(); // czy jesteśmy na kordach wyjścia
 void doEnd(); // wykonaj animację wygranej i przerzuć do następnego pliku
 int main() {
     try {
-
+        getMap();
+        beginingcoords();
+        targetcoords();
+        while(1) {
+            refreshBufor();
+            viewBufor();
+            doEvent(getEvent());
+            if(isEnd())
+                doEnd();
+        }
     } catch(...) {
         cout << "UNEXCEPTED ERROR\n";
         cin.get();
