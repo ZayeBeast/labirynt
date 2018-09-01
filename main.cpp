e033c9b9220b2a05d7fdb03d135e019f59aa2330
Nircek <nircek-2103@protonmail.com>
2018-09-01 19:38:19 +0200

    Dodanie w pojemniku funkcji set(), sort(), cont()

--- main.cpp
+++ main.cpp
@@ -20,13 +20,57 @@ char getEvent(); // pobierz znak
 void doEvent(); // wykonaj operację przypisaną do danego znaku (np WSAD)
 bool isEnd(); // czy jesteśmy na kordach wyjścia
 class pojemnik {
-    public:
     string s;
-    void sort() {
-
+    int part(int l, int r){
+        string x = get(l);
+        int i = l, j = r;
+        string w;
+        while(1) {
+            while (get(j) > x)j--;
+            while (get(i) < x)i++;
+            if (i<j) {
+                w = get(i);
+                set(i, get(j));
+                set(j, w);
+                i++;
+                j--;
+            }
+            else return j;
+        }
+    }
+    public:
+    void sort(int l=0, int r=-1) {
+        // QuickSort
+        if(r==-1)r=len()-1;
+        int q;
+        if (l < r) {
+            q = part(l, r);
+            sort(l, q);
+            sort(q+1, r);
+        }
+    }
+    public:
+    size_t len() {
+        size_t i = 0;   // koniec wyszukanego elementu
+        size_t x = 0;   // id wyszukanego elementu
+        while((i=s.find(";", i)+1))++x;
+        return x;
     }
     void push(string ns) {
         s +=  ns + ";";
+    }
+    void set(size_t ix, string ns) {//zwraca ":" jeżeli nie ma elementu o takim ID
+        size_t i = 0;   // koniec wyszukanego elementu
+        size_t j = 0;   // początek wyszukanego elementu
+        size_t x = 0;   // id wyszukanego elementu
+        while((i=s.find(";", i)+1)) {
+            if(ix==x) {
+                s=s.substr(0,j)+ns+s.substr(i-1);
+                break;
+            }
+            ++x;
+            j=i;
+        }
     }
     size_t search(string q) {
         size_t i = 0;   // koniec wyszukanego elementu
@@ -51,7 +95,10 @@ class pojemnik {
             ++x;
             j=i;
         }
-
+        return ":";
+    }
+    string cont() {
+        return s;
     }
 };
 bool doEnd() { // wykonaj animację wygranej i przerzuć do następnego pliku
@@ -66,13 +113,14 @@ bool doEnd() { // wykonaj animację wygranej i przerzuć do następnego pliku
     //TODO: MrDarkness19: a jak wszystko ok to zwróć true
 }
 int main() {
-    string a;
     pojemnik p;
     p.push("a");
     p.push("b");
     p.push("c");
     p.push("d");
-    cout<<p.s;
     cout<<p.search("d");
+    p.set(1, "e");
+    p.sort();
+    cout<<p.cont();
     return 69;
 }
