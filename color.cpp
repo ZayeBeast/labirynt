fda31fe93f0dd1012f4c01879c9824350df0b193
Nircek <nircek-2103@protonmail.com>
2018-10-19 19:11:29 +0200

    isModeAvailable

--- color.cpp
+++ color.cpp
@@ -1,7 +1,28 @@
 #include "color.h"
+#ifdef _WIN32
+#include <windows.h>
+#endif
 color_mode g_color_mode;
+bool ansichecked, ansiavailable;
+void testansi() {
+    
+}
 bool isModeAvailable(color_mode cm) {
-
+    switch(cm) {
+        case NO:
+            return true;
+        case ANSI:
+            if(ansichecked)
+                return ansiavailable;
+            testansi();
+            return ansiavailable;
+        case WIN:
+            #ifdef _WIN32
+            return true;
+            #else
+            return false;
+            #endif
+    }
 }
 void setColor(color c, surface s) {
 
