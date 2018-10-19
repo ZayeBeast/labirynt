5c61b724ed6ad501543a79d00cf93d72951b72d9
Nircek <nircek-2103@protonmail.com>
2018-10-19 21:07:54 +0200

    Dodanie switch...case'a

--- color.cpp
+++ color.cpp
@@ -35,29 +35,32 @@ bool isModeAvailable(color_mode cm) {
     }
 }
 void setColor(color c, surface s) {
-    if(g_color_mode==NO)
-        return;
-    if(g_color_mode==ANSI) {
-        bool b=false, bs=(s==FG);
-        char cc;
-        string cs;
-        switch(c) {
-            case BBLACK:    b=true; case BLACK:    cc='0'; break;
-            case BRED:      b=true; case RED:      cc='1'; break;
-            case BGREEN:    b=true; case GREEN:    cc='2'; break;
-            case BYELLOW:   b=true; case YELLOW:   cc='3'; break;
-            case BBLUE:     b=true; case BLUE:     cc='4'; break;
-            case BMAGENTA:  b=true; case MAGENTA:  cc='5'; break;
-            case BCYAN:     b=true; case CYAN:     cc='6'; break;
-            case BWHITE:    b=true; case WHITE:    cc='7'; break;
-        }
-        switch(bs&(b<<1)) {
-            case 0b00: cs="3";  break;
-            case 0b01: cs="4";  break;
-            case 0b10: cs="9";  break;
-            case 0b11: cs="10"; break;
-        }
-        cout<<"\33["<<cs<<cc<<'m';
+    switch(g_color_mode) {
+        case NO:
+            return;
+            break;
+        case ANSI:
+            bool b=false, bs=(s==FG);
+            char cc;
+            string cs;
+            switch(c) {
+                case BBLACK:    b=true; case BLACK:    cc='0'; break;
+                case BRED:      b=true; case RED:      cc='1'; break;
+                case BGREEN:    b=true; case GREEN:    cc='2'; break;
+                case BYELLOW:   b=true; case YELLOW:   cc='3'; break;
+                case BBLUE:     b=true; case BLUE:     cc='4'; break;
+                case BMAGENTA:  b=true; case MAGENTA:  cc='5'; break;
+                case BCYAN:     b=true; case CYAN:     cc='6'; break;
+                case BWHITE:    b=true; case WHITE:    cc='7'; break;
+            }
+            switch(bs&(b<<1)) {
+                case 0b00: cs="3";  break;
+                case 0b01: cs="4";  break;
+                case 0b10: cs="9";  break;
+                case 0b11: cs="10"; break;
+            }
+            cout<<"\33["<<cs<<cc<<'m';
+            break;
     }
 }
 
