3fe6ae2c9ab8146f7233b04bebf8a20a39fbb48e
MrDarkness19 <wilczy.szaniec04@gmail.com>
2018-11-22 19:35:40 +0100

    Dodanie funkcji calibrate do menu

--- main.cpp
+++ main.cpp
@@ -17,7 +17,6 @@ int main() {
               cout<<(unsigned int)ch<<' ';
             }
         }
-        calibrate();//TODO: wsadzić to do menu
         if(DEBUG) {
             cout<<"Naciskaj kolejne klawisze strzalek, zeby przetestowac kalibracje.\n";
             while(1) {
@@ -30,16 +29,21 @@ int main() {
         srand (time(NULL));
         do{
     clear_screen();
-    cout<<"Witaj w naszej amatorskiej grze \"Labirynt\"."<<endl<<endl<<endl;
-    cout<<"+---------MENU---------+"                   <<endl;
-    cout<<"| Wybierz:             |"                   <<endl;
-    cout<<"| 1. - Kampania        |"                   <<endl;
-    cout<<"| 2. - Losowa mapa     |"                   <<endl;
-    cout<<"| 3. - Wyswietl Tworcow|"                   <<endl;
-    cout<<"| 4. - Wyjdz           |"                   <<endl;
-    cout<<"+----------------------+"                   <<endl;
+    cout<<"         Witaj w naszej amatorskiej grze \"Labirynt\"."<<endl<<endl<<endl;
+    cout<<"   ################################################"       <<endl;
+    cout<<"   #                                              #"       <<endl;
+    cout<<"    #       |------------MENU------------|       # "       <<endl;
+    cout<<"     #      | Wybierz:                   |      #  "       <<endl;
+    cout<<"      #     | 1. - Kampania              |     #   "       <<endl;
+    cout<<"       #####| 2. - Losowa mapa           |#####    "       <<endl;
+    cout<<"      #     | 3. - Wyswietl tworcow      |     #   "       <<endl;
+    cout<<"     #      | 4. - Kalibracja klawiszy   |      #  "       <<endl;
+    cout<<"    #       | 5. - Wyjdz                 |       # "       <<endl;
+    cout<<"   #        |------------MENU------------|        #"       <<endl;
+    cout<<"   ################################################"       <<endl<<endl<<endl;
 
-    cout<<"Wybieram: ";
+
+    cout<<"         Wybieram: ";
     cin>>wybor;
     bool won=false;
     size_t i = (size_t)-1;
@@ -77,6 +81,10 @@ switch(wybor)
     break;
 
     case 4:
+        calibrate();
+    break;
+
+    case 5:
         clear_screen();
         cout<<"Nastapi wyjscie z programu"<<endl;
         getchar();
